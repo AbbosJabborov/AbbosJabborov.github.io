a28ec932fc9e396e49c4890ce4f44b43ec835de3
Hex Forge <abbosjabborov12@gmail.com>
2026-01-30 23:51:30 +0500

    Update: Render api calling website added to React

--- backend/personal/personal/settings.py
+++ backend/personal/personal/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://abbosjabborov.github.io",
 ]
 
 ROOT_URLCONF = "personal.urls"
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://your-render-app.onrender.com",
     },
     allowedHosts: ["unconverging-daxton-pedagogically.ngrok-free.dev"],
   },
