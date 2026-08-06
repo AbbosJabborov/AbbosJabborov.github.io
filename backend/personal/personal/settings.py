082c46cc95be9dea7b68346642ebae72dd9c6c66
Abbos Jabborov <abbosjabborov12@gmail.com>
2026-08-06 10:45:51 +0500

    Update: added claive.uz to allowed list

--- backend/personal/personal/settings.py
+++ backend/personal/personal/settings.py
@@ -19,7 +19,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = str(
     config(
         "ALLOWED_HOSTS",
-        default="127.0.0.1,localhost,app,django_backend",
+        default="127.0.0.1,localhost,app,django_backend,claive.uz,www.claive.uz,.claive.uz",
     )
 ).split(",")
 
@@ -58,7 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     for origin in str(
         config(
             "CORS_ALLOWED_ORIGINS",
-            default="http://localhost:3000,http://localhost:5173,https://abbosjabborov.github.io,https://claive.abbosjabborov12.workers.dev",
+            default="http://localhost:3000,http://localhost:5173,https://abbosjabborov.github.io,https://claive.abbosjabborov12.workers.dev,https://claive.uz,https://www.claive.uz",
         )
     ).split(",")
     if origin.strip()
@@ -69,7 +69,7 @@ CSRF_TRUSTED_ORIGINS = [
     for origin in str(
         config(
             "CSRF_TRUSTED_ORIGINS",
-            default="https://abbosjabborov.github.io,https://unconverging-daxton-pedagogically.ngrok-free.dev,https://claive.abbosjabborov12.workers.dev,https://api-personal.169.58.100.190.sslip.io",
+            default="https://abbosjabborov.github.io,https://unconverging-daxton-pedagogically.ngrok-free.dev,https://claive.abbosjabborov12.workers.dev,https://api-personal.169.58.100.190.sslip.io,https://claive.uz,https://www.claive.uz",
         )
     ).split(",")
     if origin.strip()
